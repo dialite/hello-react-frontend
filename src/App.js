119a3ab34636b69aaaabb703c7c93b0f8a4cbe15
dialite <raymondolisa01@gmail.com>
2023-03-30 13:36:24 +0100

    Modify index.js to display contents

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Learn React</div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const greetingHeading = "Random Greeting!";
+const refreshMsg = "Refresh this page to get a different greeting!";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <h1>{greetingHeading}</h1>
+    <h4>{refreshMsg}</h4>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
